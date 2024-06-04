18dac7aa9b933eb1357e2cca129b9e23aaba3cd0
Benzazoua <billalben252@gmail.com>
2024-06-04 10:17:18 +0100

    Save To Local Storage
    - refactor code
    - delete unnecessary code
    - use debounce
    - add .map bootstrap file for better debugging
    - add favicon

--- js/app.js
+++ js/app.js
@@ -1,37 +1,74 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+"use strict";
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
-    var frame = document.getElementById("preview-window").contentWindow.document;
+const $htmlTextarea = document.getElementById("htmlCode");
+const $cssTextarea = document.getElementById("cssCode");
+const $jsTextarea = document.getElementById("jsCode");
+const previewWindow = document.getElementById("preview-window");
+
+// Retrieve the code from local storage or set defaults
+const defaultCode = {
+    html: "<div>\n\n</div>",
+    css: "<style>\n\n</style>",
+    js: "<script>\n\n</script>",
+};
+
+// get the code from local storage
+const savedCode = JSON.parse(localStorage.getItem("code")) || defaultCode;
+
+$htmlTextarea.value = savedCode.html;
+$cssTextarea.value = savedCode.css;
+$jsTextarea.value = savedCode.js;
+
+function showPreview() {
+    const htmlCode = $htmlTextarea.value;
+    const cssCode = $cssTextarea.value;
+    const jsCode = $jsTextarea.value;
+
+    const frame = previewWindow.contentWindow.document;
     frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
+    frame.write(`<!DOCTYPE html>${htmlCode}${cssCode}${jsCode}`);
     frame.close();
+
+    // save the code in local storage
+    localStorage.setItem("code", JSON.stringify({ html: htmlCode, css: cssCode, js: jsCode })
+    );
 }
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+const $tabsBtn = document.querySelectorAll("[data-tab-btn]");
+let $lastActiveTabBtn = $tabsBtn[0];
+let $lastActiveTabContent = document.querySelector("[data-tab-content]");
+
+$tabsBtn.forEach(($btnTab) => {
+    $btnTab.addEventListener("click", function () {
+        $lastActiveTabContent.classList.remove("active");
+        $lastActiveTabBtn.classList.remove("active");
+
+        const $tabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        $lastActiveTabContent = $tabContent;
+        $lastActiveTabBtn = this;
+    });
+});
+
+// Debounce function to limit the rate of calling showPreview
+
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
-    }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
-    }
-}
+// Debounce the showPreview function
+const debouncedShowPreview = debounce(showPreview, 300);
+
+$htmlTextarea.addEventListener("input", debouncedShowPreview);
+$cssTextarea.addEventListener("input", debouncedShowPreview);
+$jsTextarea.addEventListener("input", debouncedShowPreview);
+
+// Initial call to showPreview
+showPreview();
